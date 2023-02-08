5b031445685713a60c38519bcfa56d58c287cfce
shaxa <shaxanuriddinov333@gmail.com>
2023-02-08 16:08:34 +0600

    first commit

---
+++ src/test/java/kz/careerguidance/CareerGuidanceApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.careerguidance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerGuidanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
